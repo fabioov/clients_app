4ba5f4e053233820db24077257c266210351a7d0
Fabio  Santos <fabioov@hotmail.com>
2023-08-24 14:15:43 -0300

    Http Client Implemented

---
+++ webapp/controller/FabiosLib/HttpClient.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/base/ManagedObject"
+], function(
+	ManagedObject
+) {
+	"use strict";
+
+	return ManagedObject.extend("clientsapp.controller.FabiosLib.HttpClient", {
+        request: function (url, method, contentType, requestData) {
+            const FETCH_OPTIONS = {
+              method: method,
+              headers: {
+                "Content-Type": contentType,
+              },
+              body: JSON.stringify(requestData),
+            };
+      
+            return fetch(url, FETCH_OPTIONS)
+              .then((response) => response.json())
+              .catch((error) => {
+                console.error(`HTTP Request Error: ${error}`);
+                throw error;
+              });
+          },
+	});
+});
